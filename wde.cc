23c37a591dbae1ac2a5c738afbd4a8ef6019f91d
Andreas Schärtl <andreas.schaertl@fau.de>
2017-01-16 13:53:52 +0100

    It works now

--- wde.cc
+++ wde.cc
@@ -1,4 +1,6 @@
 #include <sys/inotify.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
 #include <unistd.h>
 
 #include <cerrno>
@@ -7,22 +9,33 @@
 #include <cstring>
 
 #include <map>
+#include <vector>
 
 
-int main(int argc, char **argv)
+static const char *cmd;  // Name of the program, i.e. argv[0] if available
+
+
+static int init(int argc, char **argv, int &fd, std::map<int, char *> &watching)
 {
-    int inotifyfd;
-    std::map <int, const char*> watched;
+    // argv[0]
+
+    if (argc > 0) {
+	cmd = argv[0];
+    } else {
+	cmd = "wde";
+    }
+
+    // Syntax
 
     if (argc <= 1) {
-	fprintf(stderr, "%s: Missing arguments\n", argv[0]);
+	fprintf(stderr, "%s: Missing arguments\n", cmd);
 	return EXIT_FAILURE;
     }
 
     // Initialize inotify
 
-    if ((inotifyfd = inotify_init()) == -1) {
-	fprintf(stderr, "%s: Init failed: %s\n", argv[0], strerror(errno));
+    if ((fd = inotify_init()) == -1) {
+	fprintf(stderr, "%s: Init failed: %s\n", cmd, strerror(errno));
 	return EXIT_FAILURE;
     }
 
@@ -32,68 +45,95 @@ int main(int argc, char **argv)
 	int watchfd;
 	uint32_t mask = IN_CREATE | IN_DELETE | IN_ONLYDIR;
 
-	if ((watchfd = inotify_add_watch(inotifyfd, argv[i], mask)) == -1) {
-	    fprintf(stderr, "%s: Adding %s failed: %s\n", argv[0], argv[i], strerror(errno));
+	if ((watchfd = inotify_add_watch(fd, argv[i], mask)) == -1) {
+	    fprintf(stderr, "%s: Adding %s failed: %s\n", cmd, argv[i], strerror(errno));
 	    return EXIT_FAILURE;
 	}
 
-	watched.insert(std::make_pair(watchfd, argv[i]));
+	watching.insert(std::make_pair(watchfd, argv[i]));
     }
 
-    // Now keep reporting changes
+    return EXIT_SUCCESS;
+}
 
-    static const size_t INITIAL_BUFSIZE = sizeof(struct inotify_event) + 256;
-    size_t bufsize = INITIAL_BUFSIZE;
 
-    while (true) {
+static int print_changes(int fd, const std::map<int, char *> watching)
+{
+    // select(2) blocks until fd is ready for reading
+
+    {
+	fd_set set;
+	FD_ZERO(&set);
+	FD_SET(fd, &set);
+
+	if (select(fd + 1, &set, NULL, NULL, NULL) == -1) {
+	    fprintf(stderr, "%s: select(2) failed: %s", cmd, strerror(errno));
+	    return EXIT_FAILURE;
+	}
+    }
+
+    // Find out how many bytes we may read. We assume that Linux never
+    // gives us less than the full struct to read.
+
+    unsigned long bytes_ready;
+
+    if (ioctl(fd, FIONREAD, &bytes_ready) == -1) {
+	fprintf(stderr, "%s: ioctl(2) failed: %s", cmd, strerror(errno));
+	return EXIT_FAILURE;
+    }
+
+    // Read from fd and print out the changes
+
+    {
 	ssize_t rres;
-	uint8_t buf[bufsize];
+	uint8_t *buffer = new uint8_t[bytes_ready];
 
-	// Try reading from the inotify socket
-
-	rres = read(inotifyfd, buf, sizeof(buf));
-
-	// Reading failed
+	rres = read(fd, buffer, bytes_ready);
 
 	if (rres == -1) {
-	    if (errno == EINVAL) {
-		// EINVAL means the buffer was too small
-		bufsize += 256;
-		continue;
-	    } else {
-		fprintf(stderr, "%s: Cannot read ionotify descriptor: %s\n", argv[0], strerror(errno));
-		return EXIT_FAILURE;
-	    }
+	    fprintf(stderr, "%s: Cannot read(2) ionotify fd: %s\n", cmd, strerror(errno));
+	    return EXIT_FAILURE;
+	} else if ((size_t) rres != bytes_ready) {
+	    fprintf(stderr, "%s: Expected %lu bytes, got %ld", cmd, bytes_ready, rres);
+	    return EXIT_FAILURE;
 	}
 
-	// Reading succeeded
-	// We should have gotten at least one event
-	// XXX: What if we read too few bytes?
+	uint8_t *bufpos = buffer;
 
-	struct inotify_event *event = (struct inotify_event *) buf;
-
-	while (event != NULL) {
-	    // len = 0 makes no sense because we are only watching dirs
-
-	    if (event->len == 0) {
-		fprintf(stderr, "%s: Unexpected ionotify behavior\n", argv[0]);
-		return EXIT_FAILURE;
-	    }
-
-	    // Output the change
+	while (bufpos < buffer + bytes_ready) {
+	    struct inotify_event *event = (struct inotify_event *) bufpos;
 
 	    if (event->mask & IN_CREATE) {
 		printf("-> %s\n", event->name);
 	    } else if (event->mask & IN_DELETE) {
 		printf("<- %s\n", event->name);
 	    } else {
-		fprintf(stderr, "%s: Unexpected ionotify behavior\n", argv[0]);
+		fprintf(stderr, "%s: Unexpected event mask 0x%x\n", cmd, event->mask);
 		return EXIT_FAILURE;
 	    }
 
-	    // There may still be another event hidden in that buffer!
+	    bufpos = bufpos + sizeof(*event) + event->len;
+	}
 
-	    event = NULL;
+	delete buffer;
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+int main(int argc, char **argv)
+{
+    int fd;
+    std::map<int, char*> watching;
+
+    if (init(argc, argv, fd, watching) != EXIT_SUCCESS) {
+	return EXIT_FAILURE;
+    }
+    
+    while (true) {
+	if (print_changes(fd, watching) != EXIT_SUCCESS) {
+	    return EXIT_FAILURE;
 	}
     }
 }
